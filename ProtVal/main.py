dc74c74ba99566895f7d951f844ce8774a30d54d
Sebastiaan Groot <sebastiaangroot@gmail.com>
2013-09-04 20:29:43 +0200

    Working dynamic plugin loading with python

--- ProtVal/main.py
+++ ProtVal/main.py
@@ -4,5 +4,10 @@ Created on Sep 4, 2013
 @author: Sebastiaan
 '''
 
+import glob
+
+def testfunct():
+    python_files = 5
+    
 if __name__ == '__main__':
-    print('Hello World!')
+    testfunct() 
---
+++ modtest/modules/mod_tcp_client.py
@@ -0,0 +1,8 @@
+def getModuleName():
+	return "TCP Client Validator"
+
+def getModuleDescription():
+	return "A TCP server to validate TCP clients against"
+
+def initMod():
+	print("Inside TCP Client Validator")
