2b40e04eb273a6bf6e73170941afc4d7acd76b0b
Sebastiaan Groot <sebastiaangroot@gmail.com>
2013-09-05 09:27:44 +0200

    Replaced the dummy scripts in the main ProtVal directory with a working modloader with user interaction

---
+++ ProtVal/__main__.py
@@ -0,0 +1,60 @@
+import os
+import re
+
+#Returns true if the number can be cast to an integer. False otherwise
+def is_integer(string):
+	try:
+		int(string)
+		return True
+	except ValueError:
+		return False
+
+#Returns a string list of all files in the ./modules directory starting with mod_ and ending with .py
+def get_modnames(directory):
+	modnames = []
+	for item in os.listdir(directory):
+		if re.match(r'^mod_.*\.py$', item):
+			modnames.append(item[0:-3])
+	return modnames
+
+#Obtains a list of all ProtVal modules and attempts to load them
+def main():
+	modnames = get_modnames(os.path.dirname(os.path.realpath(__file__)) + "/modules")
+	loaded_modules = []
+
+	for modname in modnames:
+		#Attempt to load the module and check if it contains the required functions
+		try:
+			module = __import__('modules.%s' % modname, fromlist=[modname])
+			if hasattr(module, "getModuleName") and hasattr(module, "getModuleDescription") and hasattr(module, "initMod"):
+				loaded_modules.append(__import__('modules.%s' % modname, fromlist=[modname]))
+		except ImportError:
+			print("Error importing %s" % modname)
+	
+	if len(loaded_modules) <= 0:
+		print("No modules found. Exiting...")
+		os.sys.exit()
+	else:
+		#Display all module names and descriptions
+		print("Modules found:")
+
+		i = 1
+		for module in loaded_modules:
+			print(str(i) + ". " + module.getModuleName() + " - " + module.getModuleDescription())
+			i += 1
+
+		#Try to get the user to pick a number corresponding to one of the modules printed above.
+		#Then convert the number to the corresponding list index.
+		choice = 0
+		while True:
+			answer = input("Please enter a module to use: ")
+			if is_integer(answer):
+				choice = int(answer)
+				if choice > 0 and choice <= len(loaded_modules):
+					choice -= 1
+					break
+
+		loaded_modules[choice].initMod()
+
+if __name__ == "__main__":
+	main()		
---
+++ ProtVal/modules/mod_tcp_server.py
@@ -0,0 +1,8 @@
+def getModuleName():
+	return "TCP Server Validator"
+
+def getModuleDescription():
+	return "A TCP client to validate against TCP servers"
+
+def initMod():
+	print("Inside TCP Server Validator")
