8a4091505ab476291a3fe3c79342ffb49ab07e12
Wouter Miltenburg <wouter.miltenburg@hva.nl>
2013-09-11 22:31:26 +0200

    Moved old dns module to deprecated directory and created a new file for the dns module.

--- ProtVal/modules/mod_dns_client.py
+++ ProtVal/modules/mod_dns_client.py
@@ -6,46 +6,11 @@ def getModuleDescription():
 
 def initMod():
     print("Inside DNS Client validator")
-    main()
 
 import socket
-import dns.resolver
 
-    
-    
 def main():
-    input_user = input("Which DNS records do you want to test?")
-    host_by_name = socket.gethostbyname(input_user)
-    print('Information received from local DNS:', host_by_name)
-    
-    print("Now we are going to verify the answer using Google DNS")
-    
-    my_resolver = dns.resolver.Resolver()
-    my_resolver.nameservers = ['8.8.8.8']
-    
-    answer = my_resolver.query(input_user)
-    print(type(my_resolver.nameservers))
-    print(type(answer))
-    
-    
+    print("Main function is being executed")
 
-    for answers in answer:
-        print(answers)
-    
-    if str(host_by_name) == str(answer[0]):
-        print("Answer is the same")
-    else:
-        print("Answer is not the same. This doesn't have to mean that your settings are incorrect.")
-        print("Some websites can be reached using different IP-adresses")
-        
-    print("Check MX records:")
-
-    answer = my_resolver.query(input_user, "MX")
-    for answers in answer:
-        print(answers)
-
-    
-    
-            
-if __name__ == "__main__":
-    main()        
+if __name__ == '__main__':
+    main()
