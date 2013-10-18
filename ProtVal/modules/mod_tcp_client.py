89cc0b2e322cdcaed36bf7ffd2d2fc8d842b6cd1
Sebastiaan Groot <sebastiaangroot@gmail.com>
2013-10-18 09:36:53 +0200

    Cleaned repo for delivery

--- ProtVal/modules/mod_tcp_client.py
+++
@@ -1,8 +0,0 @@
-def getModuleName():
-	return "TCP Client Validator"
-
-def getModuleDescription():
-	return "A dummy TCP Client Validator"
-
-def initMod():
-	print("Inside TCP Client Validator")
--- ProtVal/modules/mod_tcp_server.py
+++
@@ -1,8 +0,0 @@
-def getModuleName():
-	return "TCP Server Validator"
-
-def getModuleDescription():
-	return "A dummy TCP Client Validator"
-
-def initMod():
-	print("Inside TCP Server Validator")
